9b4ea781cfcedcd02bfbdc56fb9479b70a41e2bc
smd995 <smd99540@gmail.com>
2025-04-14 16:54:11 +0900

    MovieList css, StarRating added

--- components/MovieList.js
+++ components/MovieList.js
@@ -1,21 +1,27 @@
 import Link from "next/link";
+import styles from "./MovieList.module.css";
+import StarRating from "./StarRating";
 
-export default function MovieList({ movies }) {
+export default function MovieList({ className = "", movies }) {
   return (
-    <ul>
+    <ul className={`${styles.movieList} ${className}`}>
       {movies.map((movie) => (
         <li key={movie.id}>
           <Link href={`/movies/${movie.id}`}>
-            <img src={movie.posterUrl} alt={movie.title} />
+            <img
+              className={styles.poster}
+              src={movie.posterUrl}
+              alt={movie.title}
+            />
           </Link>
-          <div>
-            <h2>{movie.title}</h2>
-            <div>
+          <div className={styles.info}>
+            <h2 className={styles.title}>{movie.title}</h2>
+            <div className={styles.date}>
               {movie.date} ・ {movie.country}
             </div>
-            <div>
-              {movie.starRating}
-              <span>{movie.starRating}</span>
+            <div className={styles.starRatingContainer}>
+              <StarRating value={movie.starRating} />
+              <span className={styles.starRating}>{movie.starRating}</span>
             </div>
           </div>
         </li>
