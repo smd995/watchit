7ce7656a7dc9a3ec55ebfabd86d5e2d8aa098016
smd995 <smd99540@gmail.com>
2025-04-15 05:33:35 +0900

    button, button link added & not found page added

---
+++ components/Button.js
@@ -0,0 +1,7 @@
+import styles from "./Button.module.css";
+
+export default function Button({ className = "", as, ...props }) {
+  return (
+    <button className={`${styles.button} ${className}`} {...props}></button>
+  );
+}
