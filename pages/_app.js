38c17e72716fa766effb0bbf77b75e2dac460097
smd995 <smd99540@gmail.com>
2025-04-17 11:34:53 +0900

    dark mode added

--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,17 @@
 import Container from "@/components/Container";
 import Header from "@/components/Header";
+import { ThemeProvider } from "@/lib/ThemeContext";
 import "@/styles/globals.css";
 
 export default function App({ Component, pageProps }) {
   return (
     <>
-      <Header />
-      <Container page>
-        <Component {...pageProps} />
-      </Container>
+      <ThemeProvider>
+        <Header />
+        <Container page>
+          <Component {...pageProps} />
+        </Container>
+      </ThemeProvider>
     </>
   );
 }
--- pages/settings.js
+++ pages/settings.js
@@ -1,11 +1,30 @@
+import Dropdown from "@/components/Dropdown";
+import { useTheme } from "@/lib/ThemeContext";
 import styles from "@/styles/Settings.module.css";
 
 export default function Settings() {
+  const { theme, setTheme } = useTheme();
+
+  function handleDropdownChange(name, value) {
+    const nextTheme = value;
+    setTheme(nextTheme);
+  }
+
   return (
     <>
       <h1 className={styles.title}>설정</h1>
       <section className={styles.section}>
         <h2 className={styles.sectionTitle}>테마 설정</h2>
+        <Dropdown
+          className={styles.dropdown}
+          name="theme"
+          value={theme}
+          options={[
+            { label: "다크", value: "dark" },
+            { label: "라이트", value: "light" },
+          ]}
+          onChange={handleDropdownChange}
+        />
       </section>
     </>
   );
